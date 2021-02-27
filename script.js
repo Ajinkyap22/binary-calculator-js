dcdbbe3ded149375fd28e4a3fd12bda9984f65b4
Ajinkyap22 <palaskarap15@gmail.com>
2021-02-27 15:24:23 +0530

    better ui & keyboard support

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
-// Get all buttons
-const [
-  btn0,
-  btn1,
-  btnClr,
-  btnEql,
-  btnSum,
-  btnSub,
-  btnMul,
-  btnDiv,
-] = document.querySelectorAll(".buttonClass");
+"use strict";
+
+// VARIABLES
 
 // Store all buttons except clear & equal in an array
-const btnsArr = [btn0, btn1, btnSum, btnSub, btnMul, btnDiv];
+const btnsArr = document.querySelectorAll(".buttonClass");
 
 const result = document.querySelector("#res");
 
-//Function to display operations
-const action = function (e) {
-  const btn = e.target || e.srcElement;
+const [btnClr, btnDel, btnEql] = document.querySelectorAll(".displayClass");
 
-  result.textContent += btn.innerHTML;
-};
+// EVENT LISTNERS
 
 // Loop over all buttons to display the operations
 btnsArr.forEach((btn) => (btn.onclick = action));
 
 // Clear results
-btnClr.onclick = () => (result.innerHTML = "");
+btnClr.addEventListener("click", clear);
 
 // Display result of operations
-btnEql.onclick = function () {
-  // 1. Convert binary operands to decimal
-  let expression = result.innerHTML;
+btnEql.addEventListener("click", evaluate);
+
+// Delete last value from the input
+btnDel.addEventListener("click", deleteNum);
+
+// Take input from keyboard
+window.addEventListener("keydown", keyboardSupport);
+
+// FUNCTIONS
+
+// function to display operations
+function action(e) {
+  const btn = e.target || e.srcElement;
+
+  // Limit the input size
+  if (result.textContent.length >= 15) return;
+
+  result.textContent += btn.textContent;
+}
+
+// function to evaluate expressions
+function evaluate() {
+  let expression = result.textContent;
   const nums = /(\d+)/g;
+
   expression = expression.replace(nums, function (match) {
     return parseInt(match, 2);
   });
-  result.innerHTML = Math.floor(eval(expression)).toString(2);
-};
+
+  result.textContent = Math.floor(eval(expression)).toString(2);
+}
+
+// function to delete last entered input
+function deleteNum() {
+  result.textContent = result.textContent.slice(0, -1);
+}
+
+// clear calc screen
+function clear() {
+  result.textContent = "0";
+}
+
+function keyboardSupport(e) {
+  // if 0 or 1 is pressed or an operator is pressed
+  if ((e.key >= 0 && e.key <= 1) || "+-/*".includes(e.key)) {
+    result.textContent += e.key;
+  }
+  // if = or enter is pressed
+  if (e.key === "=" || e.key === "Enter") {
+    evaluate();
+  }
+  // if backspace is pressed
+  if (e.key === "Backspace") {
+    deleteNum();
+  }
+  // if escape is pressed then clear all
+  if (e.key === "Escape") {
+    clear();
+  }
+}
